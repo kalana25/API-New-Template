048ab1ddd6fdf5aa22a4b1584f3cc40815dfac7b
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-07-20 20:32:03 +0100

    Add simple usecase

---
+++ Core/Interfaces/UseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public abstract class UseCase
+    {
+        public UseCase()
+        {
+
+        }
+    }
+}
---
+++ UseCases/General/Styles/GetCustomStyles/GetCustomStylesUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using AutoMapper;
+using Models;
+using Repositories;
+
+namespace UseCases.General.Styles.GetCustomStyles
+{
+    public class GetCustomStylesUseCase : UseCase
+    {
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public bool CustomStyle { get; set; }
+
+        public GetCustomStylesUseCase(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Style>> Execute()
+        {
+            return await unitOfWork.Styles.GetCustomStyles(CustomStyle);
+        }
+    }
+}
